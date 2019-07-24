1132fe32edf72f87f7c036ccf5fa6bb280c5cd86
slagga <957766762@qq.com>
2019-07-24 15:30:06 +0800

    fix setting files

---
+++ python-btcrpc/package/setting.example.py
@@ -0,0 +1,7 @@
+DINGDING_URL = 'https://oapi.dingtalk.com/robot/send?access_token=f714e22c7e07fc9ed2d2f9f2450de46ff7862229972e8aeed04adb68f0a6c249'
+
+RPC_HOST = '127.0.0.1'
+RPC_PORT = '16124'
+RPC_USER = 'username'
+RPC_PASS = 'mS2SZpPCZZVZwSP8EOCyMmGxqFNyPoKSUdiYSpcLVQE='
+TIMEOUT = 120
